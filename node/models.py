f1ded3e44c042db46320057f88348fe582ed261f
Jimmy <datamikolaj@protonmail.com>
2024-04-04 11:29:49 +0200

    Create endpoints for node

--- node/models.py
+++ node/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+
 from utils.models import TimestampedModel
+
 
 class Node(TimestampedModel):
     node_name = models.CharField(max_length=20)
     qth_locator = models.CharField(max_length=10)
     last_seen = models.DateTimeField()
-
--- node/views.py
+++ node/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from http import HTTPStatus
 
-# Create your views here.
+from ninja import Router
+
+from node.controllers import list_nodes_controller, get_node_controller, create_node_controller, update_node_controller, \
+    delete_node_controller
+from node.schemas import NodeOut, NodeIn
+
+router = Router(tags=["Nodes"])
+
+
+@router.get("/node/", response={HTTPStatus.OK: NodeOut})
+def list_nodes(request):
+    return list_nodes_controller()
+
+
+@router.get("/node/{node_id}/", response={HTTPStatus.OK: NodeOut})
+def get_node(request, node_id: int):
+    return get_node_controller(node_id)
+
+
+@router.post("/node/")
+def create_node(request, payload: NodeIn):
+    return create_node_controller(payload)
+
+
+@router.put("/node/{node_id}")
+def update_node(request, payload: NodeIn, node_id: int):
+    return update_node_controller(payload, node_id)
+
+
+@router.delete("/node/{node_id}")
+def delete_node(request, node_id: int):
+    delete_node_controller()
+    return {"sucess": True}
