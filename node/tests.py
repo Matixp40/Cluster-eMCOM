5e691b29351c7d5943c3348128e92eb167d3ad03
Jimmy <datamikolaj@protonmail.com>
2024-04-05 16:23:00 +0200

    Start testing app. List works

--- node/tests.py
+++ node/tests.py
@@ -1,3 +1,31 @@
+from datetime import datetime
+
 from django.test import TestCase
 
-# Create your tests here.
+from node.models import Node
+
+
+class NodeTestCase(TestCase):
+    def test_list_nodes(self):
+        test_node = Node.objects.create(node_name='test', last_seen=datetime.now())
+        response = self.client.get('/api/node/')
+        assert (
+                response.status_code == 200
+        ), "Response code should be 200"
+
+        content = response.json()
+        assert (
+                type(content) == list
+        ), "Response content should be an list"
+        assert (
+                content[0]['node_name'] == test_node.node_name
+        ), "Node name should be other"
+
+    def test_get_node(self):
+        pass
+
+    def test_delete_node(self):
+        pass
+
+    def test_update_node(self):
+        pass
--- node/views.py
+++ node/views.py
@@ -9,27 +9,27 @@ from node.schemas import NodeOut, NodeIn
 router = Router(tags=["Nodes"])
 
 
-@router.get("/node/", response={HTTPStatus.OK: NodeOut})
+@router.get("", response={HTTPStatus.OK: list[NodeOut]})
 def list_nodes(request):
     return list_nodes_controller()
 
 
-@router.get("/node/{node_id}/", response={HTTPStatus.OK: NodeOut})
+@router.get("{node_id}/", response={HTTPStatus.OK: NodeOut})
 def get_node(request, node_id: int):
     return get_node_controller(node_id)
 
 
-@router.post("/node/")
+@router.post("")
 def create_node(request, payload: NodeIn):
     return create_node_controller(payload)
 
 
-@router.put("/node/{node_id}")
+@router.put("{node_id}")
 def update_node(request, payload: NodeIn, node_id: int):
     return update_node_controller(payload, node_id)
 
 
-@router.delete("/node/{node_id}")
+@router.delete("{node_id}")
 def delete_node(request, node_id: int):
     delete_node_controller()
     return {"sucess": True}
