c23fab7a2bbdad83de93bf59fb02c5346699405c
Matixp40 <148278625+Matixp40@users.noreply.github.com>
2024-04-26 06:18:45 +0200

    Update tests.py

--- connection/tests.py
+++ connection/tests.py
@@ -1,3 +1,31 @@
+from datetime import datetime
+
 from django.test import TestCase
 
-# Create your tests here.
+from connection.models import Connection  # Assuming 'Connection' is the model name
+
+
+class ConnectionTestCase(TestCase):
+    def test_list_connections(self):
+        test_connection = Connection.objects.create(connection_name='test', last_seen=datetime.now())
+        response = self.client.get('/api/connection/')
+        assert (
+            response.status_code == 200
+        ), "Response code should be 200"
+
+        content = response.json()
+        assert (
+            type(content) == list
+        ), "Response content should be a list"
+        assert (
+            content[0]['connection_name'] == test_connection.connection_name
+        ), "Connection name should match"
+
+    def test_get_connection(self):
+        pass
+
+    def test_delete_connection(self):
+        pass
+
+    def test_update_connection(self):
+        pass
