b2a4f55771f11cdf52f92395c63519fb3d35961b
Matixp40 <148278625+Matixp40@users.noreply.github.com>
2024-04-26 06:21:01 +0200

    Update schemas.py

--- api/connection/schemas.py
+++ api/connection/schemas.py
@@ -1,1 +1,17 @@
+from ninja import ModelSchema
+
+from connection.models import Connection
+
+
+class ConnectionIn(ModelSchema):
+    class Meta:
+        model = Connection
+        fields = ['caller', 'called', 'ConnectionType']
+        fields_optional = ['caller', 'called', 'ConnectionType']
+
+
+class ConnectionOut(ModelSchema):
+    class Meta:
+        model = Connection
+        fields = ['caller', 'called', 'ConnectionType', 'created', 'updated']
 
