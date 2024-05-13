d80782546fb4b7efc04b73251216e96e8261a66a
Matixp40 <148278625+Matixp40@users.noreply.github.com>
2024-05-13 13:03:38 +0200

    Update tests.py

--- connection/tests.py
+++ connection/tests.py
@@ -18,16 +18,42 @@ class ConnectionTestCase(TestCase):
             type(content) == list
         ), "Response content should be a list"
         assert (
-            content[0]['connection_name'] == test_connection.connection_name
+            content[0]['logged_by'] == test_connection.logged_by
         ), "Connection name should match"
 
-    def test_post_connection(self, request):
+    def test_create_connection(self):
+        body= {"logged_by": "Rzeszow"}
+        response = self.client.post(
+            "/api/connection/", data=body, content_type="connection/json" 
+        )
+         assert response.status_code == 200, "Status code should be 200"
+        assert response.json()["logged_by"] == "Rzeszow"
+        assert (
+            response.json()["created"] is not None
+        ), "Created should be automatically set"
         
-    def test_get_connection(self, request):
-        pass
+    def test_get_connection(self):
+         response = self.client.get("/api/connection/1/")
+        assert response.status_code == 200, "Response code should be 200"
 
-    def test_delete_connection(self, request):
-        pass
+        content = response.json()
+        assert (
+            content["logged_by"] == self.test_connection.logged_by
+        ), 
 
-    def test_update_connection(self, request):
-        pass
+    def test_delete_connection(self):
+         response = self.client.delete("/api/connection/1/")
+        assert response.status_code == 200, "Response code should be 200"
+
+ content = response.json()
+        assert content["success"], "Nothing should be here"
+        assert list(Connection.objects.all()) == []
+
+    def test_update_connection(self):
+        body = {"called": "KNML"}
+        response = self.client.put(
+            "/api/node/1", data=body, content_type="application/json"
+        )
+        assert response.status_code == 200, "Response code should be 200"
+        assert response.json()["qth_locator"] == "KNML", "Response from put is inproper"
+        
